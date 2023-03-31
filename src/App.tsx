10c09a86e9b9b64e8d1ebbaa84519f014fcdda64
alexbushi <alexbush@udel.edu>
2023-03-30 21:03:13 -0400

    Three column layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorScheme="teal" size="sm">
-      Button
-    </Button>
+    <Grid templateColumns="repeat(3, 1fr)" padding={1}>
+      <GridItem w="100%" h="10" bg="green.500">
+        Menu
+      </GridItem>
+      <GridItem w="100%" h="10" bg="blue.500">
+        Main
+      </GridItem>
+      <GridItem w="100%" h="10" bg="yellow.500">
+        Suggestions
+      </GridItem>
+    </Grid>
   );
 }
 
