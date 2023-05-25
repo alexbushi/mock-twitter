ea169bcd9fd3fc341aad855eb105bcee5ee5b255
alexbushi <alexbush@udel.edu>
2023-05-24 23:35:15 -0400

    retrigger build

--- src/data/currentEvents.ts
+++ src/data/currentEvents.ts
@@ -1,5 +1,5 @@
 const currentEvents = [
-  { category: "sports", subject: "IPL 2023", num_of_tweets: "53.8K" },
+  { category: "sports", subject: "IPL 2023", num_of_tweets: "54.8K" },
   { category: "weather", subject: "Tornado Warning", num_of_tweets: "6,0315" },
   {
     category: "politics",
